436e83b6ac89ea37f05b5f7e034398bb7b8fbd90
Sandeep Mehta <sandeep797671@gmail.com>
2025-12-17 14:44:12 +0530

    feat: add request interceptor to axios instance for authorization token

--- src/API/axiosInstance.js
+++ src/API/axiosInstance.js
@@ -1,8 +1,17 @@
+import { getAuth } from "@clerk/clerk-react";
 import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BACKEND_URL,
   withCredentials: true,
+});
+
+axiosInstance.interceptors.request.use(async (config) => {
+  const token = await window.Clerk?.session?.getToken();
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 // Response interceptor
